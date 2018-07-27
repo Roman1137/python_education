a69c389ff92add739d4c3a65563c172e71b17e22
Roman <poma.borodavka@gmail.com>
2018-07-28 00:18:37 +0300

    finished first lesson

---
+++ PythonEducation/FirstSteps/printUsingVariable.py
@@ -0,0 +1,4 @@
+print("value1", "value2") #value1 и value2 будут выведены через пробел т.к это 2 разных аргумента
+
+name = input('What is your Name?') #What is your Name? будет выведено
+print('Hello', name)
